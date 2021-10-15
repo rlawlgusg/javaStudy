be815a30fc65a3ac4ef09bd28abae8629e0104d0
rlawlgusg <91641715+rlawlgusg@users.noreply.github.com>
2021-10-15 18:22:13 +0900

    Add files via upload

--- day1008/Exam.java
+++ day1008/Exam.java
@@ -1,73 +1,52 @@
 import java.util.Scanner;
-class MyUtil{ //클래스하나에 여러개 메소드 생성가능..했네 
-	int correctAnswers(char []answer, char []student_answer){
-		int cnt = 0;
-		for(int i =0;i<answer.length;i++){
-			if(answer[i] == student_answer[i]){
-				cnt++;
-			}
-		}
-		return cnt;
-	}
+class Exam{ 
+		boolean flag = true;
+		int correct = 0;
+		int incorrect = 0;
 
-	int incorrectAnswers(char []answer, char []student_answer){
-		return 10 - correctAnswers(answer, student_answer);//메소드안에서 메소드 호출가능
-	}
+char []right_answer = {'c','a','a','a','c','d','b','a','c','b'};
+char []answer = new char[10];
+Scanner sc = new Scanner(System.in);
 
-	boolean isPassed(char []answer, char []student_answer){
-		return correctAnswers(answer, student_answer) >= 7; 
+
+public static void main(String[] args){
+	for(int i = 0; i < answer.length; i++){//for문은 클래스안에서 사용불가 꼭 메소드로 감싸야한다
+	System.out.printf("%d번 문제의 답",i+1);
+	answer[i] = sc.next.charAt[0];
 	}
+}
+
+static int correctAnswers(char []rightanswer,char[] answer){
+	for(int i=0;i<answer.length;i++){
+		if(answer[i] == rightanswer[i]){
+					correct ++;}
+				return correct;}
+	
+static int incorrectAnswers(char []rightanswer,char[] answer){incorrect ++;
+return incorrect;}
+
+
+static boolean isPassed(char []rightanswer,char[] answer)
+{if(correctAnswers(rightanswer, answer) >= 7) return true;
+else return false;	}
 
 }
 
 
-class Exam
+class ExamResult
 {
-	public static void main(String[] args) 
-	{
-		String []question = {
-		"대한민국의 수도는?",
-		"미국의 수도는",
-		"모나코의 수도는",
-		"브라질의 수도는",
-		"스위스의 수도는",
-		"싱가포르의 수도는",
-		"영국의 수도는",
-		"인도의 수도는",
-		"캐나다의 수도는",
-		"터키의 수도는"};
+	
 
-		String []view = {
-		"a)아크라 b)서울 c)코펜하겐 d)야렌",
-		"a)워싱턴 b)서울 c)코펜하겐 d)야렌",
-		"a)아크라 b)서울 c)모나코 d)야렌",
-		"a)아크라 b)서울 c)코펜하겐 d)브라질리아",
-		"a)베른 b)서울 c)코펜하겐 d)야렌",
-		"a)아크라 b)싱가포르 c)코펜하겐 d)야렌",
-		"a)아크라 b)런던 c)코펜하겐 d)야렌",
-		"a)뉴델리 b)서울 c)코펜하겐 d)야렌",
-		"a)아크라 b)서울 c)오타와 d)야렌",
-		"a)앙카라 b)서울 c)코펜하겐 d)야렌"};
 
-		char []answer = {'b','a','c','d','a','b','b','a','c','a'};
-		char []student_answer = new char[10];
+		boolean result = Exam.isPassed();
+		int correct = Exam.correctAnswer();
+		int incorrect = Exam.incorrectAnswer();
 
-		Scanner sc = new Scanner(System.in);
-		for(int i =0 ; i<question.length ; i++){
-			System.out.println(question[i]);
-			System.out.print(view[i]);
-			student_answer[i] = sc.next().charAt(0);
-		}
-
-		MyUtil util = new MyUtil();
-		int cnt = util.correctAnswers(answer, student_answer); //메소드 호출!
-
-		System.out.printf("정답수: %d\n",cnt);
-		System.out.printf("틀린답수: %d\n",util.incorrectAnswers(answer,student_answer));
-		if (util.isPassed(answer, student_answer)){
-			System.out.printf("합격여부: %s\n","합격");		
-		}else
-			System.out.printf("합격여부: %d\n","불합격");
 		
+
+		System.out.printf("정답 횟수 : %d\n",correct);
+		System.out.printf("오답 횟수 : %d\n",incorrect);
+		System.out.printf(result);
+
 	}
 }
