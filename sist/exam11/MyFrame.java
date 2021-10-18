fadd29ef3428d0280645d17213b87733de21fc3a
rlawlgusg <91641715+rlawlgusg@users.noreply.github.com>
2021-10-18 19:04:16 +0900

    Delete sist directory

--- sist/exam11/MyFrame.java
+++
@@ -1,9 +0,0 @@
-package com.sist.exam11;
-
-import javax.swing.JFrame;
-
-public class MyFrame extends JFrame {
-	public MyFrame(String title) {
-		super(title);
-	}
-}
--- sist/exam11/MyMath.java
+++
@@ -1,5 +0,0 @@
-package com.sist.exam11;
-//final 클래스를 상속 받을 수 없음
-public class MyMath extends Math {
-
-}
